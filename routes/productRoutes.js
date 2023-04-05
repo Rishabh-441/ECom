310c3cca2a4230736aca1c168e8e71d00eb9c80d
Rishabh-441 <90997234+Rishabh-441@users.noreply.github.com>
2023-04-05 14:32:59 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../Models/product');
 
-
+router.get('/', (req, res) => {
+    router.redirect('/products');
+});
 router.get('/products', async (req, res) => {
     const products = await Product.find({});
 
